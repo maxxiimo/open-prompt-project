7ea0e4dd25013d127e494e1459a3c8df8b244aa3
nickumia <nickumia6@gmail.com>
2024-02-18 20:55:59 -0500

    new: initial frontend copy

---
+++ frontend/src/jsx/home_main.js
@@ -0,0 +1,166 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Container from '@mui/material/Container';
+import Typography from "@material-ui/core/Typography";
+
+const ImageBackdrop = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  background: '#000',
+  opacity: 0.5,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageIconButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  display: 'block',
+  padding: 0,
+  borderRadius: 0,
+  height: '40vh',
+  [theme.breakpoints.down('md')]: {
+    width: '100% !important',
+    height: 100,
+  },
+  '&:hover': {
+    zIndex: 1,
+  },
+  '&:hover .imageBackdrop': {
+    opacity: 0.15,
+  },
+  '&:hover .imageMarked': {
+    opacity: 0,
+  },
+  '&:hover .imageTitle': {
+    border: '4px solid currentColor',
+  },
+  '& .imageTitle': {
+    position: 'relative',
+    padding: `${theme.spacing(2)} ${theme.spacing(4)} 14px`,
+  },
+  '& .imageMarked': {
+    height: 3,
+    width: 18,
+    background: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  },
+}));
+
+const images = [
+  {
+    url: '/static/img/20231018_180624_optimized.jpg',
+    title: 'NLP Research',
+    width: '30%',
+    link: 'nlp',
+  },
+  {
+    url: '/static/img/20231020_142059_optimized.jpg',
+    title: 'London 2023',
+    width: '40%',
+    link: 'london',
+  },
+  {
+    url: '/static/img/20231014_143134_optimized.jpg',
+    title: 'Kumia',
+    width: '30%',
+    link: 'kumia',
+  },
+  {
+    url: '/static/img/20231221_204447_optimized.jpg',
+    title: 'New York 2023',
+    width: '40%',
+    link: 'new_york',
+  },
+  {
+    url: '/static/img/vlogging.png',
+    title: 'Vlogging!',
+    width: '20%',
+    link: 'https://www.youtube.com/@nickumia',
+  },
+  {
+    url: '/static/img/road_trip_main.jpg',
+    title: 'Road Trip Across America',
+    width: '40%',
+    link: 'spiritual_tech',
+  },
+  {
+    url: '/static/img/financial_background.jpg',
+    title: 'Financial Resources',
+    width: '50%',
+    link: '',
+    // link: 'financial',
+  },
+  {
+    url: '/static/img/spiritual_background.jpg',
+    title: 'Hinduism + Spiritual Resources',
+    width: '50%',
+    link: 'spiritual',
+  },
+];
+
+export default function HomeMain() {
+  return (
+    <React.Fragment>
+    <Container component="section" sx={{ mt: 8, mb: 4 }}>
+      <Typography variant="h4" marked="center" align="center" component="h2">
+        Connecting People through Technology for a Better Tomorrow
+      </Typography>
+      <Box sx={{ mt: 8, display: 'flex', flexWrap: 'wrap' }}>
+        {images.map((image) => (
+          <ImageIconButton
+            key={image.title}
+            href={image.link}
+            style={{
+              width: image.width,
+            }}
+          >
+            <Box
+              sx={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center 40%',
+                backgroundImage: `url(${image.url})`,
+              }}
+            />
+            <ImageBackdrop className="imageBackdrop" />
+            <Box
+              sx={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                color: 'common.white',
+              }}
+            >
+              <Typography
+                component="h3"
+                variant="h6"
+                color="inherit"
+                className="imageTitle"
+              >
+                {image.title}
+                <div className="imageMarked" />
+              </Typography>
+            </Box>
+          </ImageIconButton>
+        ))}
+      </Box>
+    </Container>
+    </React.Fragment>
+  );
+}
