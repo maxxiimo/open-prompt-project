5be4c7b7c4627e0c47a81099823168e570ac059f
nickumia <nickumia6@gmail.com>
2024-02-23 23:26:37 -0500

    new: one more call to /create

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -177,7 +177,7 @@ export const NavBar = () => {
           </Search>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" color="inherit" href="/create">
+            <IconButton size="large" color="inherit" href='/create?__technique_data=[{"id": "prompt", "label": "Prompt", "placeholder": "Can you tell me about the creation of blackholes?", "helperText": "Specific information or an instruction you want the model to process."}, {"id": "answer", "label": "Expected Answer", "placeholder": "Black holes are regions of spacetime where the gravitational force is so strong that nothing, not even light, can escape from it. They are created when a very massive star dies and its core collapses in on itself, forming a singularity of infinite density. The intense gravity of the singularity pulls in all the matter and radiation around it, creating the black hole.", "helperText": "The expected response or elements of the response from the LLM."}]&__technique_name="Zero-Shot Prompting"'>
               <Tooltip title="New Prompt">
                 <AddIcon />
               </Tooltip>
